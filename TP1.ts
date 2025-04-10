a4acf21dedb6d030a5ad917dc4f4378b25873459
Tobias <tobiaspsp@hotmail.com>
2025-04-10 20:53:04 -0300

    Primer producto de la lista

--- TP1.ts
+++ TP1.ts
@@ -1,1 +1,17 @@
-""
+//Datos simulados: 
+
+const productos = [  
+    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },  
+    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },  
+    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" }, 
+    { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },  
+    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }  
+    ];
+
+//Parte 1: Acceso a elementos y recorrido de arrays
+
+//1. Acceder a propiedades individuales:
+//Mostrá en la consola el nombre del primer producto de la lista.
+
+console.log("Primer producto de la lista:", productos[0].nombre);
+
